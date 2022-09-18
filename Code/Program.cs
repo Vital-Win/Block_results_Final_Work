9acae8836d965df56780e4b0e79b16b519f3f9c3
Vitaliy <vinogradovvitaliy2013@gmail.com>
2022-09-18 17:35:47 +0300

    Code

---
+++ Code/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+void CreateArray(string[] firstStringArray, string[] secondStringArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstStringArray.Length; i++)
+    {
+        if (firstStringArray[i].Length <= 3)
+        {
+            secondStringArray[count] = firstStringArray[i];
+            count++;
+        }
+    }
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+    Console.Write("]");
+}
+
+
+string[] firstStringArray = { "hello", "2", "world", ":-)" };
+string[] secondStringArray = new string[firstStringArray.Length];
+
+CreateArray(firstStringArray, secondStringArray);
+PrintArray(secondStringArray);
